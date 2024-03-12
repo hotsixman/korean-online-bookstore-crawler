db9e3733ba9a4674edad0837464357a119477971
hotsixisbest <galactagun12@gmail.com>
2024-03-12 13:05:38 +0900

    readme 수정, index 수저d

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import * as aladin from './src/store/aladin/crawler';
+import * as yes24 from './src/store/yes24/crawler';
 
-const kobc = {
-    store:{
-        aladin
-    }
+class KOBCFunction extends Function{
+    store:any;
+}
+
+const kobc = new KOBCFunction();
+kobc.store = {
+    aladin,
+    yes24
 }
 
 export default kobc;
